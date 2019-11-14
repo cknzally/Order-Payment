ea4d59b03b35b54b7a0d32661d62d7a66da51f8a
cknzally <cynzally@yahoo.fr>
2019-11-14 15:15:35 +0100

    creation classe

---
+++ Order.cs
@@ -0,0 +1,98 @@
+﻿using Newtonsoft.Json;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace FilRouge
+{
+    public class Order
+    {
+        #region Attributs
+        private long  idOrder;
+        private int quantity;
+        private long idBike;
+        private float sumFreetax;
+        private DateTime date;
+        private long idShop;
+        private long idCustemer;
+        private List<Payment> payMode;
+        private float discount;
+        private int loyaltyPoint;
+        //private List<Payment> payMode;
+
+       
+       
+
+
+
+        #endregion
+
+        #region Properties 
+        public long IdOrder
+        {
+            get { return idOrder; }
+            set { idOrder = value; }
+        }
+
+        public int Quantity
+
+        {
+            get { return quantity; }
+            set { quantity = value; }
+        }
+
+        public long IdBike
+
+        {
+            get { return idBike; }
+            set { idBike = value; }
+        }
+
+        public float Sumfreetax
+
+        {
+            get { return sumFreetax; }
+            set { sumFreetax = value; }
+        }
+
+        public DateTime Date
+        {
+            get { return date; }
+            set { date = value; }
+        }
+
+        public long IdShop
+
+        {
+            get { return idShop; }
+            set { idShop = value; }
+        }
+
+        public long IdCustemer
+
+        {
+            get { return idCustemer; }
+            set { idCustemer = value; }
+        }
+
+        public float Discount
+
+        {
+            get { return discount; }
+            set { discount = value; }
+        }
+
+        public int LoyaltyPoint
+
+        {
+            get { return loyaltyPoint; }
+            set { loyaltyPoint = value; }
+        }
+
+       
+
+        #endregion
+    }
+}
---
+++ Payment.cs
@@ -0,0 +1,35 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace FilRouge
+{
+    class Payment
+    {
+        #region Attributs
+        private string name;
+        private float amount;
+
+
+        #endregion
+
+        #region Properties
+        public string Name
+
+        {
+            get { return name; }
+            set { name = value; }
+        }
+
+        public float Amount
+
+        {
+            get { return amount; }
+            set { amount = value; }
+        }
+
+        #endregion
+    }
+}
